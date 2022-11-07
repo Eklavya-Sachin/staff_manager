5a2bcf830964e324058b5cd59fbb96d6e67b8e06
Sachin Kumar <ojaz07860@gmail.com>
2022-11-07 22:12:40 +0530

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# staff_manager
+
+A flutter project to add staff details & show all staff details in other navigation bar.
---
+++ android/app/src/main/kotlin/com/staff/manager/staff_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.staff.manager.staff_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
