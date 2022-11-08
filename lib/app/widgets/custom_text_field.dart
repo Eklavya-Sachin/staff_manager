061cbb8357b1a53c860f6e7a5329201787341070
Sachin Kumar <ojaz07860@gmail.com>
2022-11-09 01:16:30 +0530

    Code updated only.

--- lib/app/widgets/custom_text_field.dart
+++ lib/app/widgets/custom_text_field.dart
@@ -17,7 +17,6 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      autovalidateMode: AutovalidateMode.onUserInteraction,
       controller: controller,
       keyboardType: keyboardType,
       cursorColor: Colors.black,
