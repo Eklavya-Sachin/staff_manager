5e6c16c25876528fbc239a205c7bc209493edf0a
Sachin Kumar <ojaz07860@gmail.com>
2022-11-09 01:54:06 +0530

    Code updated only.

--- lib/app/screens/home.dart
+++ lib/app/screens/home.dart
@@ -27,7 +27,7 @@ class _HomeState extends State<Home> {
       child: Scaffold(
         body: IndexedStack(
           index: _currentIndex,
-          children: const [
+          children: [
             AddStaffs(),
             ShowStaffs(),
           ],
